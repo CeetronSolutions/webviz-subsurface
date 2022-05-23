43b9e980630d00170c866667c9604c6b386b81cc
Ruben <ruben.thoms@ceetronsolutions.com>
2022-05-23 17:17:03 +0200

    Removed unnecessary import.

--- webviz_subsurface/plugins/_volumetric_analysis_refactored/_callbacks.py
+++ webviz_subsurface/plugins/_volumetric_analysis_refactored/_callbacks.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, Optional
 
 from dash import ALL, Input, Output, State, callback, callback_context, no_update
 from dash.exceptions import PreventUpdate
